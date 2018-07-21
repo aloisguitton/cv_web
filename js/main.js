8fed27916f231bee154769b7fab87838711e57cb
aloisguitton <aloisguitton@orange.fr>
2018-07-22 00:04:27 +0200

    2018-07-22 00:04:27

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-var app = angular.module('cv_web', ["ngRoute"]);
-
-app.config(function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    templateUrl: "index.html"
-  })
-  .when("/formation", {
-    templateUrl: "html/formation"
-  })
-  .otherwise({
-    redirectTo: '/'
-  })
-})
+var app = angular.module('cv_web', ["ngRoute"])
+  .config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl: "index.html"
+    })
+    .when("/formation", {
+      templateUrl: "html/formation"
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+  });
