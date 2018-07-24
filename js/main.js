753863cca78141efcd809b7cf3d0e5aaf3c38892
aloisguitton <aloisguitton@orange.fr>
2018-07-24 11:38:29 +0200

    2018-07-24 11:38:29

--- js/main.js
+++ js/main.js
@@ -11,3 +11,9 @@ var app = angular.module('cv_web', ["ngRoute"])
       redirectTo: '/'
     })
   });
+
+  var telechargement = document.getElementbyId("telechargement");
+  telechargement.style.cursor = 'pointer';
+  telechargement.onclick = function(){
+    alert("ok");
+  };
